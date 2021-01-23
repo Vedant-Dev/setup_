a694120c0de90f2253a2f8a69d675a668b9718bb
Vedant Singh <53736139+Vedant-Dev@users.noreply.github.com>
2021-01-23 19:29:25 +0530

    Completed random_name.py

--- random_name.py
+++ random_name.py
@@ -1,4 +1,8 @@
 
 import secrets
-a =  "abcdefghijklmnopqrstuvwxyz".upper()
-print(secrets.choice(apl))
+letter_list =  "abcdefghijklmnopqrstuvwxyz".upper()
+length = input("Enter number of letter")
+final_name = ''
+for _ in range(length):
+  final_name = final_name + secrets.choice(letter_list)
+ print(final_name)
